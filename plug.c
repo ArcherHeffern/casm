8b3cdaa9c5e137a2cfa3e8bd09d289170ce7f0e9
ArcherHeffern <heffernarcher@gmail.com>
2024-10-30 22:46:37 -0400

    futures, styles, complex animations, and much more!

--- plug.c
+++ plug.c
@@ -9,16 +9,24 @@
 #define CELL_SIZE 1024
 #define MEMORY_SIZE 64
 #define STORAGE_SIZE 64
+#define MAX_FUTURES 256
 #define MAX_ANIMATIONS 256
+#define MAX_STYLE_OVERRIDES 256
 #define RESET_DELAY 0.1
 
 typedef struct State State;
 typedef struct RenderInfo RenderInfo;
+typedef struct Future Future;
+typedef struct StyleOverride StyleOverride;
 typedef struct Animation Animation;
 typedef enum EasingFunction EasingFunction;
+typedef enum OverrideType OverrideType;
 
 // Rendering Constants
+#define HEADER_SIZE 24
+#define TEXT_SIZE 12
 #define SLIDE_IN_TIME 0.5 // Seconds
+#define MEMORY_CELL_SET_VALUE_TIME 1
 #define X_PADDING 40
 
 struct RenderInfo {
@@ -38,7 +46,9 @@ struct RenderInfo {
 	int cell_gap;
 	Color cell_color;
 	int scroll_speed;
+	Future* futures[MAX_FUTURES];
 	Animation* animations[MAX_ANIMATIONS]; 
+	StyleOverride* style_overrides[MAX_STYLE_OVERRIDES];
 	Rectangle memory_pointer; // Program Counter
 	Rectangle storage_pointer; 
 };
@@ -49,6 +59,12 @@ enum EasingFunction {
 	IN_N_BACK // Treats end_val as the midpoint value and returns to start_val
 };
 
+struct Future {
+	float delay; // Seconds
+	void** reference;
+	void* future_value;
+};
+
 struct Animation {
 	EasingFunction easing;
 	double percent; // 0-1 completeness
@@ -57,6 +73,18 @@ struct Animation {
 	double start_val;
 	double end_val;
 	double* value;
+	StyleOverride* style_override;
+};
+
+enum OverrideType {
+	NONE,
+	MEMORY_CELL_SIZE_MULTIPLIER
+};
+
+struct StyleOverride {
+	OverrideType type;
+	int id;
+	double style;	
 };
 
 void AnimationDbg(Animation* a) {
@@ -79,15 +107,33 @@ void plug_init(char* filename);
 State* plug_pre_reload();
 void plug_update();
 
+// ============
+// StyleOverrides
+// ============
+StyleOverride* StyleOverrideCreate(OverrideType type, int id, float style);
+double* StyleOverrideGet(OverrideType type, int id); // Returns reference to style or NULL
+void StyleOverrideDestroy(StyleOverride* style_override);
+
+// ============
+// Futures
+// ============
+void CreateFuture(float delay, void** reference, void* future_value);
+void FutureStep(Future* future, float dt);
+void FutureFree(Future* future);
 
 // ============
 // Animations
 // ============
-void CreateAnimation(double end, double* value, EasingFunction easing, float delay);
+void CreateAnimation(double end, double* value, EasingFunction easing, float duration, float delay, StyleOverride* style_override);
 void AnimationStep(Animation* animation, float dt);
+void AnimationFree(Animation* animation);
+
+// ============
+// Complex Animations
+// ============
 void SetActiveMemoryCell(int cell, EasingFunction easing, float delay);
 void SetActiveStorageCell(int cell, EasingFunction easing, float delay);
-void AnimationFree(Animation* animation);
+void SetMemoryCellValue(int cell, char* value, float delay);
 
 // ============
 // Runners
@@ -96,15 +142,18 @@ void Run();
 void StartVisualisation();
 bool Step();
 bool StepAnimations();
+bool StepFutures();
 
 // ============
 // Renderers
 // ============
 void Render();
 void RenderMemory();
+void RenderMemoryCell(int i);
 void RenderRegisters();
 void RenderRegister(int i);
 void RenderStorage();
+void RenderStorageCell(int i);
 void RenderControls();
 
 // ============
@@ -141,6 +190,11 @@ float ParametricBlend(float t) {
     return sqr / (2.0f * (sqr - t) + 1.0f);
 }
 
+float SinInAndBack(float t) {
+	// Divinely Inspired (Tsoding)
+	return sinf(t*PI);
+}
+
 
 // ============
 // Hot Reloading
@@ -165,7 +219,6 @@ void plug_init(char* filename) {
 		.cell_gap = 20,
 		.cell_color = GetColor(0xcaccde),
 		.scroll_speed = 4,
-		.animations = { NULL },
 		.memory_pointer = {
 			.x = 0,
 			.y = 0,
@@ -208,36 +261,104 @@ void plug_update() {
 }
 
 // -------------
+// StyleOverrides 
+// -------------
+StyleOverride* StyleOverrideCreate(OverrideType type, int id, float style) {
+	StyleOverride** style_overrides = s->render_info.style_overrides;
+	
+	StyleOverride* style_override = (StyleOverride*)malloc(sizeof(StyleOverride));
+	style_override->type = type;
+	style_override->id = id;
+	style_override->style = style;
+
+	for (int i = 0; i < MAX_STYLE_OVERRIDES; i++) {
+		if (style_overrides[i] == NULL) {
+			style_overrides[i] = style_override;
+			return style_override;
+		}
+	}
+	assert(false && "Style Override queue is full");
+}
+
+double* StyleOverrideGet(OverrideType type, int id) {
+	for (int i = 0; i < MAX_STYLE_OVERRIDES; i++) {
+		StyleOverride* style_override = s->render_info.style_overrides[i];
+		if (style_override == NULL) {
+			continue;
+		}
+		if (style_override->type == type && style_override->id == id) {
+			return &style_override->style;
+		}
+	}
+	return NULL;
+}
+
+void StyleOverrideDestroy(StyleOverride* style_override) {
+	StyleOverride** style_overrides = s->render_info.style_overrides;
+
+	for (int i = 0; i < MAX_STYLE_OVERRIDES; i++) {
+		if (style_override == style_overrides[i]) {
+			style_overrides[i] = NULL;
+			free(style_override);
+			return;
+		}
+	}
+	assert(false && "Could not find style override");
+}
+// -------------
+// Futures 
+// -------------
+void CreateFuture(float delay, void** reference, void* future_value) {
+	RenderInfo* render_info = &s->render_info;
+
+	Future* future = (Future*)malloc(sizeof(Future));
+	future->delay = delay;
+	future->reference = reference;
+	future->future_value = future_value;
+	for (size_t i = 0; i < MAX_FUTURES; i++) {
+		if (render_info->futures[i] == NULL) {
+			render_info->futures[i] = future;
+			return;
+		}		
+	}
+	assert(false && "Future queue is full");
+}
+
+void FutureStep(Future* future, float dt) {
+	if (future->delay > 0) {
+		future->delay = MaxFloat(0, future->delay - dt);
+	}
+	if (future->delay > 0) {
+		return;
+	}
+	*future->reference = future->future_value;
+}
+
+void FutureFree(Future* future) {
+	free(future);
+}
+	
+// -------------
 // Animations
 // -------------
-void CreateAnimation(double end, double* value, EasingFunction easing, float delay) {
+void CreateAnimation(double end, double* value, EasingFunction easing, float duration, float delay, StyleOverride* style_override) {
 	RenderInfo* render_info = &s->render_info;
 	Animation* animation = (Animation*)malloc(sizeof(Animation));
 	animation->easing = easing;
 	animation->percent = 0;
-	animation->duration = SLIDE_IN_TIME; 
+	animation->duration = duration; 
 	animation->delay = delay;
 	animation->start_val = *value;
 	animation->end_val = end;
 	animation->value = value;
+	animation->style_override = style_override;
 	for (int i = 0; i < MAX_ANIMATIONS; i++) {
 		if (render_info->animations[i] == NULL) {
 			render_info->animations[i] = animation;
-			break;
+			return;
 		}
 	}
-}
-
-void SetActiveMemoryCell(int cell, EasingFunction easing, float delay) {
-	RenderInfo* render_info = &s->render_info;
-	float end = -render_info->cell_height*cell - render_info->cell_gap*cell + GetScreenHeight() / 2 - render_info->cell_height / 2;
-	CreateAnimation(end, &render_info->memory_height, easing, delay);
-}
-
-void SetActiveStorageCell(int cell, EasingFunction easing, float delay) {
-	RenderInfo* render_info = &s->render_info;
-	float end = -render_info->cell_height*cell - render_info->cell_gap*cell + GetScreenHeight() / 2 - render_info->cell_height / 2;
-	CreateAnimation(end, &render_info->storage_height, easing, delay);
+	assert(false && "Animation queue is full");
 }
 
 void AnimationStep(Animation* animation, float dt) {
@@ -249,6 +370,11 @@ void AnimationStep(Animation* animation, float dt) {
 	if (animation->delay > 0) {
 		return;
 	}
+	if (animation->duration == 0) {
+		animation->percent = 1.0F;
+		*animation->value = animation->end_val;
+		return;
+	}
 	animation->percent = MinDouble(dt / animation->duration + animation->percent, 1.0F);
 	float percent = animation->percent;
 	switch (animation->easing) {
@@ -256,6 +382,9 @@ void AnimationStep(Animation* animation, float dt) {
 			break;
 		case IN_N_OUT:
 			percent = ParametricBlend(percent);
+			break;
+		case IN_N_BACK:
+			percent = SinInAndBack(percent);
 			break;
 		default:
 			fprintf(stderr, "Unexpected easing function %d\n", animation->easing);
@@ -265,7 +394,34 @@ void AnimationStep(Animation* animation, float dt) {
 }
 
 void AnimationFree(Animation* animation) {
+	if (animation->style_override != NULL) {
+		StyleOverrideDestroy(animation->style_override);
+	}
 	free(animation);
+}
+
+// ============
+// Complex Animations
+// ============
+void SetActiveMemoryCell(int cell, EasingFunction easing, float delay) {
+	RenderInfo* render_info = &s->render_info;
+	float end = -render_info->cell_height*cell - render_info->cell_gap*cell + GetScreenHeight() / 2 - render_info->cell_height / 2;
+	CreateAnimation(end, &render_info->memory_height, easing, SLIDE_IN_TIME, delay, NULL);
+}
+
+void SetActiveStorageCell(int cell, EasingFunction easing, float delay) {
+	RenderInfo* render_info = &s->render_info;
+	float end = -render_info->cell_height*cell - render_info->cell_gap*cell + GetScreenHeight() / 2 - render_info->cell_height / 2;
+	CreateAnimation(end, &render_info->storage_height, easing, SLIDE_IN_TIME, delay, NULL);
+}
+
+void SetMemoryCellValue(int cell, char* value, float delay) {
+	// Set text after delay
+	CreateFuture(delay, (void*)&s->memory[cell], value);
+
+	// Grow and shrink cell by a percent
+	StyleOverride* style_override = StyleOverrideCreate(MEMORY_CELL_SIZE_MULTIPLIER, cell, 1);
+	CreateAnimation(1.2, &style_override->style, IN_N_BACK, 1, 0, style_override);
 }
 
 // ============
@@ -282,25 +438,31 @@ void StartVisualisation() {
 		GetScreenHeight() - 10*(render_info->register_cell_height + render_info->register_cell_gap),
 		&render_info->register_height,
 		IN_N_OUT,
-		0
+		SLIDE_IN_TIME,
+		0,
+		NULL
 	);
 	Run();
 	SetActiveStorageCell(0, IN_N_OUT, 0);
+	Run();
+	SetMemoryCellValue(0, "Goodbye World", 0);
 	Run();
 	SetActiveMemoryCell(1, IN_N_OUT, 0);
 	Run();
 	SetActiveMemoryCell(2, IN_N_OUT, 0);
 	Run();
-	SetActiveMemoryCell(3, IN_N_OUT, 1);
+	SetActiveMemoryCell(3, IN_N_BACK, 0);
+	Run();
 	return;
 	Run();
 	// CreateRegisterSeekEvent(render_info->register_cell_height+, IN_N_OUT);
-	Run();
 	CreateAnimation(
 		0, 
 		&render_info->storage_height, 
 		IN_N_OUT,
-		0
+		SLIDE_IN_TIME,
+		0,
+		NULL
 	);
 	Run();
 }
@@ -315,9 +477,31 @@ void Run() {
 
 bool Step() {
 	bool animations_left = StepAnimations();
+	bool futures_left = StepFutures();
 	HandleFileUpload();
 	Render();
-	return animations_left;
+	return animations_left || futures_left;
+}
+
+bool StepFutures() {
+	RenderInfo* render_info = &s->render_info;
+	Future** futures = render_info->futures; 
+	bool has_events = false;
+	float dt = GetFrameTime();
+	for (int i = 0; i < MAX_FUTURES; i++) {
+		Future* future = futures[i];
+		if (future == NULL) {
+			continue;
+		} 
+		has_events = true;
+		FutureStep(future, dt);
+
+		if (future->delay == 0) {
+			futures[i] = NULL;
+			FutureFree(future);
+		}
+	}
+	return has_events;
 }
 
 bool StepAnimations() {
@@ -332,12 +516,13 @@ bool StepAnimations() {
 			continue;
 		} 
 		has_events = true;
-		AnimationStep(animation, dt);
-
 		if (animation->percent == 1) {
 			animations[i] = NULL;
 			AnimationFree(animation);
+		} else {
+			AnimationStep(animation, dt);
 		}
+
 	}
 	return has_events;
 }
@@ -359,28 +544,38 @@ void RenderMemory() {
 	RenderInfo* render_info = &s->render_info;
 	char** memory = s->memory;
 
-	DrawText("Memory", X_PADDING, render_info->cell_gap, 24, render_info->font_color);
+	DrawText("Memory", X_PADDING, render_info->cell_gap, HEADER_SIZE, render_info->font_color); // Title
+	DrawRectangleLinesEx(render_info->memory_pointer, 2.0F, render_info->pc_color); // Program Counter
 	for (int i = 0; i < MEMORY_SIZE; i++) {
-		int y = render_info->memory_height + render_info->cell_height*i + render_info->cell_gap*i;
-		DrawRectangle(
-			X_PADDING,
-			y,
-			render_info->cell_width,
-			render_info->cell_height,
-			render_info->cell_color
-		);
-		char* msg = NULL;
-		asprintf(&msg, "Ox%x: %s", i*4, memory[i]);
-		DrawText(msg, X_PADDING + 20, y+render_info->cell_height/2, 12, render_info->font_color);
+		RenderMemoryCell(i);
 	}
-	DrawRectangleLinesEx(render_info->memory_pointer, 2.0F, render_info->pc_color); 
+}
+
+void RenderMemoryCell(int i) {
+	RenderInfo* render_info = &s->render_info;
+	char** memory = s->memory;
+	double* maybe_multiplier = StyleOverrideGet(MEMORY_CELL_SIZE_MULTIPLIER, i);
+	double multiplier = maybe_multiplier == NULL ? 1: *maybe_multiplier;
+
+	int y = render_info->memory_height + render_info->cell_height*i + render_info->cell_gap*i;
+	DrawRectangle(
+		X_PADDING - 0.5 * (render_info->cell_width * multiplier - render_info->cell_width),
+		y - 0.5 * (render_info->cell_height * multiplier - render_info->cell_height),
+		render_info->cell_width * multiplier,
+		render_info->cell_height * multiplier,
+		render_info->cell_color
+	);
+	char* msg = NULL;
+	asprintf(&msg, "Ox%x: %s", i*4, memory[i]);
+	float textWidth = MeasureTextEx(GetFontDefault(), msg, TEXT_SIZE, 1).x;
+	DrawText(msg, X_PADDING + render_info->cell_width/2 - textWidth/2, y+render_info->cell_height/2, TEXT_SIZE, render_info->font_color);
 }
 
 void RenderRegisters() {
 	RenderInfo* render_info = &s->render_info;
-	float textWidth = MeasureTextEx(GetFontDefault(), "Registers", 24, 1).x;
+	float textWidth = MeasureTextEx(GetFontDefault(), "Registers", HEADER_SIZE, 1).x;
 	Vector2 position = { .x=GetScreenWidth()/2 - textWidth/2, .y=render_info->cell_gap };
-	DrawTextEx(GetFontDefault(), "registers", position, 24, 1, render_info->font_color);
+	DrawTextEx(GetFontDefault(), "registers", position, HEADER_SIZE, 1, render_info->font_color);
 	for (int i = 0; i < 10; i++) {
 		RenderRegister(i);
 	}
@@ -403,36 +598,45 @@ void RenderRegister(int i) {
 		free(msg);
 		asprintf(&msg, "PC: %d", s->registers[0]);
 	}
-	DrawText(msg, x+20, y+render_info->register_cell_height/2, 12, render_info->font_color);
+	DrawText(msg, x+20, y+render_info->register_cell_height/2, TEXT_SIZE, render_info->font_color);
 }
 
 void RenderStorage() {
 	RenderInfo* render_info = &s->render_info;
 	char** storage = s->storage;
 
-	float textWidth = MeasureTextEx(GetFontDefault(), "Storage", 24, 1).x;
-	DrawText("Storage", GetScreenWidth() - X_PADDING - textWidth, render_info->cell_gap, 24, render_info->font_color);
-	for (int i = 0; i < STORAGE_SIZE; i++) {
-		int y = render_info->storage_height + render_info->cell_height*i + render_info->cell_gap*i;
-		DrawRectangle(
-			GetScreenWidth() - X_PADDING - render_info->cell_width,
-			y,
-			render_info->cell_width,
-			render_info->cell_height,
-			render_info->cell_color
-		);
-		char* msg = NULL;
-		asprintf(&msg, "Ox%x: %s", i*4, storage[i]);
-		DrawText(msg, GetScreenWidth() - X_PADDING - render_info->cell_width + 20, y+render_info->cell_height/2, 12, render_info->font_color);
-	}
+	float textWidth = MeasureTextEx(GetFontDefault(), "Storage", HEADER_SIZE, 1).x;
+	DrawText("Storage", GetScreenWidth() - X_PADDING - textWidth, render_info->cell_gap, HEADER_SIZE, render_info->font_color);
 	DrawRectangleLinesEx(render_info->storage_pointer, 2.0F, render_info->pc_color); 
+
+	for (int i = 0; i < STORAGE_SIZE; i++) {
+		RenderStorageCell(i);
+	}
+}
+
+void RenderStorageCell(int i) {
+	RenderInfo* render_info = &s->render_info;
+	char** storage = s->storage;
+	int y = render_info->storage_height + render_info->cell_height*i + render_info->cell_gap*i;
+
+	DrawRectangle(
+		GetScreenWidth() - X_PADDING - render_info->cell_width,
+		y,
+		render_info->cell_width,
+		render_info->cell_height,
+		render_info->cell_color
+	);
+	char* msg = NULL;
+	asprintf(&msg, "Ox%x: %s", i*4, storage[i]);
+	float textWidth = MeasureTextEx(GetFontDefault(), msg, TEXT_SIZE, 1).x;
+	DrawText(msg, GetScreenWidth() - X_PADDING - render_info->cell_width/2 - textWidth/2, y+render_info->cell_height/2, TEXT_SIZE, render_info->font_color);
 }
 
 void RenderControls() {
 	RenderInfo* render_info = &s->render_info;
 	char* instruction = "Drag assembly file to upload";
-	Vector2 textSize = MeasureTextEx(GetFontDefault(), instruction, 12, 1);
-	DrawText(instruction, GetScreenWidth()/2 - textSize.x/2, render_info->header_gap, 12, render_info->font_color);
+	Vector2 textSize = MeasureTextEx(GetFontDefault(), instruction, TEXT_SIZE, 1);
+	DrawText(instruction, GetScreenWidth()/2 - textSize.x/2, render_info->header_gap, TEXT_SIZE, render_info->font_color);
 	
 	float width = render_info->register_cell_width/2 * (1-render_info->register_cell_gap);
 	float height = render_info->register_cell_height + render_info->register_cell_gap;
@@ -446,12 +650,12 @@ void RenderControls() {
 	DrawRectangleRec(continue_button, render_info->button_color);
 
 	char* continue_text = "(C)ontinue";
-	textSize = MeasureTextEx(GetFontDefault(), continue_text, 12, 1);
+	textSize = MeasureTextEx(GetFontDefault(), continue_text, TEXT_SIZE, 1);
 	DrawText(
 		continue_text, 
 		continue_button.x+continue_button.width/2-textSize.x/2, 
 		continue_button.y+continue_button.height/2-textSize.y/2, 
-		12, 
+		TEXT_SIZE, 
 		render_info->font_color
 	);
 	
@@ -464,12 +668,12 @@ void RenderControls() {
 	DrawRectangleRec(step_button, render_info->button_color);
 
 	char* step_text = "(S)tep";
-	textSize = MeasureTextEx(GetFontDefault(), step_text, 12, 1);
+	textSize = MeasureTextEx(GetFontDefault(), step_text, TEXT_SIZE, 1);
 	DrawText(
 		step_text, 
 		step_button.x+step_button.width/2-textSize.x/2, 
 		step_button.y+step_button.height/2-textSize.y/2, 
-		12, 
+		TEXT_SIZE, 
 		render_info->font_color
 	);
 }
